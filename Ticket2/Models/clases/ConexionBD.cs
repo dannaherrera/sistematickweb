0b30e7095e01e5fea92c2f5b12c7b56e2d364b15
DannaHerrera <danna.paola.210702@gmail.com>
2026-05-19 17:56:24 -0600

    FIX: Reparacion y apuntar a reporsitorio correcto,ademas de incluir cadena de conexion

---
+++ Ticket2/Models/clases/ConexionBD.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SistemaTickets.Models.Clases
+{
+
+    public class ConexionBD
+    {
+
+        private readonly string _connectionString;
+        public ConexionBD()
+        {
+            _connectionString = ConfigurationManager
+                .ConnectionStrings["DefaultConnection"]
+                .ConnectionString;
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
+
+
